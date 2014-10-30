ee9cbd982365d2e5c96a3d33c5ed69088e70849c
BigKuCha <nevermore1989@vip.qq.com>
2014-10-30 22:00:03 +0800

    .

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -143,7 +143,7 @@ AppAsset::register($this);
     <div class="sidebar-shortcuts" id="sidebar-shortcuts">
         <div class="sidebar-shortcuts-large" id="sidebar-shortcuts-large">
             <button class="btn btn-success">
-                <a href="/backend/reflushmenu"><i class="icon-signal"></i></a>
+                <i class="icon-signal"></i>
             </button>
 
             <button class="btn btn-info">
