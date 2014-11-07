1b8c8e60711e721bb3369f594e225f46dea7ccc4
BigKuCha <nevermore1989@vip.qq.com>
2014-11-07 11:05:19 +0800

    test restful :1234:

--- backend/controllers/RestfulController.php
+++ backend/controllers/RestfulController.php
@@ -11,6 +11,7 @@ namespace backend\controllers;
 use Yii;
 use yii\rest\ActiveController;
 
+
 class RestfulController extends ActiveController
 {
     public $modelClass = 'backend\models\TAdmUser';
